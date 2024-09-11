23f79d9b4e092cb851f599081434512753fbb98c
frndo1203 <oliveirafer06@gmail.com>
2024-09-11 11:21:46 -0300

    fix(sum): :bug: Add import testing for test file for correct go test

--- math_test.go
+++ math_test.go
@@ -1,9 +1,11 @@
 package main
 
-func TestSoma(t, *testing.T) {
+import "testing"
+
+func TestSoma(t *testing.T) {
 	total := Soma(15, 15)
 
 	if total != 30 {
-		t.Errorf("Resultado da soma é invalido: Resultado %d. Espado: %d", total, 30)
+		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
